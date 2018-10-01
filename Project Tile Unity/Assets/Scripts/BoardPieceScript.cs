0fcea524a0213615cd4e97d74693cebe445b6ee4
Yi Su Ho <xon235@naver.com>
2018-10-01 18:30:20 +0900

    Added untracked file

---
+++ Project Tile Unity/Assets/Scripts/BoardPieceScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardPieceScript : MonoBehaviour
+{
+    public int X { get; private set; }
+    public int Y { get; private set; }
+
+    private TileScript tileAbove;
+
+    public void InitBoardPiece(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+	public void PlaceTileOver(TileScript tile, float offset)
+    {
+        tile.transform.position = transform.position + new Vector3(0, offset, 0);
+        GetComponent<BoxCollider2D>().enabled = false;
+        tileAbove = tile;
+    }
+
+    public IEnumerator DropTile(List<TileScript> tilesOnBoard, float delay)
+    {
+        if (tileAbove != null)
+        {
+            yield return new WaitForSeconds(delay);
+            if (tileAbove != null)
+            {
+                tileAbove.IsTileEnabled = true;
+                tilesOnBoard.Add(tileAbove);
+                tileAbove = null;
+            }
+        }
+    }
+
+    public void ClearAboveTile()
+    {
+        if (tileAbove != null)
+        {
+            Destroy(tileAbove.gameObject);
+            GetComponent<BoxCollider2D>().enabled = true;
+            tileAbove = null;
+        }
+    }
+}
